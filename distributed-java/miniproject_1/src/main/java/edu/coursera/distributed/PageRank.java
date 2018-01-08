e9fa8c95ecf0ea2a481527c7253d5a0d3a135d7f
Yuri Bochkarev <baltazar.bz@gmail.com>
2018-01-08 22:17:41 +0300

    distributed-java: week1: solve

--- distributed-java/miniproject_1/src/main/java/edu/coursera/distributed/PageRank.java
+++ distributed-java/miniproject_1/src/main/java/edu/coursera/distributed/PageRank.java
@@ -4,6 +4,10 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import scala.Tuple2;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Iterator;
+
 /**
  * A wrapper class for the implementation of a single iteration of the iterative
  * PageRank algorithm.
@@ -49,6 +53,27 @@ public final class PageRank {
     public static JavaPairRDD<Integer, Double> sparkPageRank(
             final JavaPairRDD<Integer, Website> sites,
             final JavaPairRDD<Integer, Double> ranks) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+
+        JavaPairRDD<Integer, Double> newRanks = sites.join(ranks).flatMapToPair(kv -> {
+            Integer websiteId = kv._1;
+            Tuple2<Website, Double> value = kv._2;
+
+            Website site = value._1;
+            Double currentRank = value._2;
+            Double rankRatioPart = currentRank / (double) site.getNEdges();
+
+            List<Tuple2<Integer, Double>> contribs = new LinkedList<Tuple2<Integer, Double>>();
+            Iterator<Integer> iter = site.edgeIterator();
+
+            while (iter.hasNext()) {
+                final int target = iter.next();
+                contribs.add(new Tuple2<Integer, Double>(target, rankRatioPart));
+            }
+            return contribs;
+        });
+
+        return newRanks.reduceByKey((Double r1, Double r2) -> r1 + r2)
+                .mapValues(v -> v * 0.85 + 0.15);
     }
 }
